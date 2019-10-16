14a5cc675f1776897f7f379a83546e7b38ab1c54
ClaireRimel <sivadiercl@gmail.com>
2019-10-16 12:53:49 +0200

    New: Add new projet to git

---
+++ Realisez-une-application-de-voyage/Realisez-une-application-de-voyage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realisez-une-application-de-voyage
+//
+//  Created by Claire on 16/10/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Realisez-une-application-de-voyage/Realisez-une-application-de-voyageTests/Realisez_une_application_de_voyageTests.swift
@@ -0,0 +1,34 @@
+//
+//  Realisez_une_application_de_voyageTests.swift
+//  Realisez-une-application-de-voyageTests
+//
+//  Created by Claire on 16/10/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import XCTest
+@testable import Realisez_une_application_de_voyage
+
+class Realisez_une_application_de_voyageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
