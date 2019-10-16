16f474826a9ad5f778fe16e4ad8ffb533102cf93
ClaireRimel <sivadiercl@gmail.com>
2019-10-16 16:47:11 +0200

    New: add WeatherViewController, TranslateViewController and ExchangeViewController

---
+++ Realisez-une-application-de-voyage/LeBaluchon/Controller/ExchangeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExchangeViewController.swift
+//  LeBaluchon
+//
+//  Created by Claire on 16/10/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
