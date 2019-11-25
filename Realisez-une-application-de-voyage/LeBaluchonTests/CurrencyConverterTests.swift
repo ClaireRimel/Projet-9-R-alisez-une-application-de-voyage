cf33839ebede3ca9859d23ed5f53cef0043bb1ba
ClaireRimel <sivadiercl@gmail.com>
2019-11-25 19:58:00 +0100

    Fix: indexing info plist test target

---
+++ Realisez-une-application-de-voyage/LeBaluchonTests/CurrencyConverterTests.swift
@@ -0,0 +1,43 @@
+//
+//  CurrencyConverterTests.swift
+//  LeBaluchonTests
+//
+//  Created by Claire on 25/11/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import XCTest
+@testable import LeBaluchon
+
+class CurrencyConverterTests: XCTestCase {
+
+    var sut: CurrencyConverter!
+    
+    override func setUp() {
+      sut = CurrencyConverter()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testInvalidInput() {
+        // Given
+        let input = "A"
+        let expectation = self.expectation(description: "Scaling")
+        
+        // When
+        sut.convert(from: input) { (result) in
+            // Then
+            XCTAssertEqual(result, .failure(.invalidInput))
+            expectation.fulfill()
+
+        }
+        //wait...
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    // Given
+          // When
+          // Then
+}
