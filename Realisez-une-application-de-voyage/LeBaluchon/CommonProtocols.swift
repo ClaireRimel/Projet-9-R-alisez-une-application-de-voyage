a7016a592534824e3d1fe9f38ce7a154554cbe50
ClaireRimel <sivadiercl@gmail.com>
2019-11-26 19:26:03 +0100

    New : add protocol RequestInterface

---
+++ Realisez-une-application-de-voyage/LeBaluchon/CommonProtocols.swift
@@ -0,0 +1,18 @@
+//
+//  CommonProtocols..swift
+//  LeBaluchon
+//
+//  Created by Claire on 26/11/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import Foundation
+
+
+//TODO: relocate common protocol
+protocol RequestInterface {
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: RequestInterface {}
